72883e428920dd57e1f72671dc67ed52498dcc3a
Tanmay Aswal <tanmayaswal80@gmail.com>
2024-05-18 10:48:56 +0530

    Added Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,8 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Signup} from "./pages/Signup";
+import {Signin} from "./pages/Signin";
+import {Dashboard} from "./pages/Dashboard";
+import {SendMoney} from "./pages/SendMoney";
 
 function App() {
 
